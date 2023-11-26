3f50d8e5095b995b8f20b80a2b43b8d4023c53c7
kapsa <75359352+yousifmohame@users.noreply.github.com>
2023-11-26 21:16:37 +0200

    Add files via upload

---
+++ questions02.js
@@ -0,0 +1,222 @@
+let questions = [
+    {
+        numb: 1,
+        question: " common approaches to ethical decision making.",
+        answer: "D. All of the above",
+        options: [
+            "A. virtue ethics approach",
+            "B. utilitarian approach",
+            "C. fairness approach",
+            "D. All of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: " _____ refers to protecting data and computer system against dishonesty or negligence of employees.",
+        answer: "A. Personnel security",
+        options: [
+            "A. Personnel security",
+            "B. Personal security",
+            "C. Physical security",
+            "D. None of these"
+        ]
+    },
+    { 
+        numb: 3,
+        question: " types of computer attacks.",
+        answer: "D. All of the above",
+        options: [
+            "A. virus",
+            "B. worm",
+            "C. Trojan horse",
+            "D. All of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: " Actions taken by Microsoft to support trustworthy computing.",
+        answer: "D. All of the above",
+        options: [
+            "A. Security",
+            "B. Privacy",
+            "C. Reliability",
+            "D. All of the above"
+        ]
+    },
+    {
+        numb: 5,
+        question: " Firewalls can be established through the use of.",
+        answer: "C. internal network and the internet",
+        options: [
+            "A. Internal network",
+            "B. the Internet",
+            "C. internal network and the internet",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 6,
+        question: " You can protect sensitive data from prying eyes using.",
+        answer: "A. File permissions",
+        options: [
+            "A. File permissions",
+            "B. File locks",
+            "C. Passwords",
+            "D. Encryption"
+        ]
+    },
+    {
+        numb: 7,
+        question: " Is a set of rules for determining moral standards or socially acceptable behavior.",
+        answer: "C. Ethics",
+        options: [
+            "A. Protocol",
+            "B. Standard",
+            "C. Ethics",
+            "D. None of the given"
+        ]
+    },
+    {
+        numb: 8,
+        question: " Cracking” comes under.",
+        answer: "C. Computer Crime",
+        options: [
+            "A. Website Service",
+            "B. Transcription",
+            "C. Computer Crime",
+            "D. Data Security"
+        ]
+    },
+    {
+        numb: 9,
+        question: " A hacker contacts your phone or E-mail and attempts to acquire your password is called.",
+        answer: "C. Phishing",
+        options: [
+            "A. Spamming",
+            "B. Spoofing",
+            "C. Phishing",
+            "D. None of these"
+        ]
+    },
+    {
+        numb:10,
+        question: " is the illegal access to the network or computer system.",
+        answer: "B. Cracking",
+        options: [
+            "A. Security",
+            "B. Cracking",
+            "C. Virus",
+            "D. Piracy"
+        ]
+    },
+    {
+        numb: 11,
+        question: " Running other software on idle computer without the knowledge of the organization is called theft of ____.",
+        answer: "C. Computer time",
+        options: [
+            "A. Software",
+            "B. Computer",
+            "C. Computer time",
+            "D. Use"
+        ]
+    },
+    {
+        numb: 12,
+        question: " Which of the following is not a way of protection?",
+        answer: "C. Piracy",
+        options: [
+            "A. Physical Security",
+            "B. Personal Security",
+            "C. Piracy",
+            "D. Personnel security"
+        ]
+    },
+    {
+        numb: 13,
+        question: " ___ is a self- replicating program that can cause damage to data and files stored on your computer.",
+        answer: "A. Virus",
+        options: [
+            "A. Virus",
+            "B. Freeware",
+            "C. Piracy",
+            "D. None of the given"
+        ]
+    },
+    {
+        numb: 14,
+        question: " Making and using duplicate hardware and software is called ___.",
+        answer: "A. Piracy",
+        options: [
+            "A. Piracy",
+            "B. Free copy",
+            "C. Copy right",
+            "D. None of these"
+        ]
+    },
+    {
+        numb: 15,
+        question: " ____ refers to software setups that permit only authorized access to the system.",
+        answer: "B. Personal security",
+        options: [
+            "A. Personnel security",
+            "B. Personal security",
+            "C. Physical security",
+            "D. None of these"
+        ]
+    },
+    {
+        numb: 16,
+        question: " The protection of hardware, facilities magnetic disks, and other illegally accessed, stolen, damaged or destroyed items refers to ____.",
+        answer: "A. Physical security",
+        options: [
+            "A. Physical security",
+            "B. Personal security",
+            "C. Personnel security",
+            "D. None of the given"
+        ]
+    },
+    {
+        numb: 17,
+        question: " ________ helps to classify arguments and situations, better understand a cyber-crime and helps to determine appropriate actions.",
+        answer: "A. Cyber-ethics",
+        options: [
+            "A. Cyber-ethics",
+            "B. social ethics",
+            "C. Cyber-bullying",
+            "D. corporate behavior"
+        ]
+    },
+    {
+        numb: 18,
+        question: " Which responsibilities fall under professional responsibilities?",
+        answer: "D. Above all",
+        options: [
+            "A. Confidentiality",
+            "B. Computer ethics",
+            "C. Environmental ethics",
+            "D. Above all"
+        ]
+    },
+    {
+        numb: 19,
+        question: " Which one does fall under computer ethics?",
+        answer: "D. Above all",
+        options: [
+            "A. Computers can be used for robbery, crime and blackmailing others.",
+            "B. Hacking is used to access private information",
+            "C. Leak of such information of individual leads to harassments in terms of repeated phone calls",
+            "D. Above all"
+        ]
+    },
+    {
+        numb: 20,
+        question: " _________ refers to the principles of right and wrong behaviour.",
+        answer: "A. Moral",
+        options: [
+            "A. Moral",
+            "B. Oral",
+            "C. Coral",
+            "D. None"
+        ]
+    },
+];
