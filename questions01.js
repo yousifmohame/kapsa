785e2cafa498f4b935b3076c1fae6304f3418f9b
kapsa <75359352+yousifmohame@users.noreply.github.com>
2023-11-26 21:08:25 +0200

    Add files via upload

---
+++ questions01.js
@@ -0,0 +1,135 @@
+let questions = [
+    {
+        numb: 1,
+        question: " Management is a social process means that:",
+        answer: "C. it takes place through people.",
+        options: [
+            "A. it aims at achieving pre-determined objectives.",
+            "B. it is influenced by various changes.",
+            "C. it takes place through people.",
+            "D. it is aided by computers."
+        ]
+    },
+    {
+        numb: 2,
+        question: " Which of the following is not one of the old manager's practices?",
+        answer: "B. working with and for group members.",
+        options: [
+            "A. making most decisions alone.",
+            "B. working with and for group members.",
+            "C. hiding information.",
+            "D. considering himself as a boss or controller."
+        ]
+    },
+    {
+        numb: 3,
+        question: " The formal study of management began as a part or reflection of the:",
+        answer: "C. industrial revolution.",
+        options: [
+            "A. communication revolution.",
+            "B. knowledge revolution.",
+            "C. industrial revolution.",
+            "D. Russian revolution."
+        ]
+    },
+    {
+        numb: 4,
+        question: " The “Contemporary Developments School” focused on:",
+        answer: "D. external environment.",
+        options: [
+            "A. work.",
+            "B. workers.",
+            "C. internal environment.",
+            "D. external environment."
+        ]
+    },
+    {
+        numb: 5,
+        question: " According to the “Classical School” assumptions:",
+        answer: "C. man is rational and has his own financial and physical needs.",
+        options: [
+            "A. man is social and has his own psychological and social needs.",
+            "B. any organization is considered as an open system.",
+            "C. man is rational and has his own financial and physical needs.",
+            "D. all of the above."
+        ]
+    },
+    {
+        numb: 6,
+        question: " One of the major criticisms of the “Behavioral School” is that it:",
+        answer: "B. focused on internal variables of the organization and neglected variables of the external environment.",
+        options: [
+            "A. dealt with people as machines.",
+            "B. focused on internal variables of the organization and neglected variables of the external environment.",
+            "C. ignored the efficiency and power considerations inside the organization.",
+            "D. considered financial incentives as the most important factor, which motivates people to perform their work."
+        ]
+    },
+    {
+        numb: 7,
+        question: " Frederick Taylor is referred to as:",
+        answer: "B. the father of scientific management.",
+        options: [
+            "A. the father of the modern management theory.",
+            "B. the father of scientific management.",
+            "C. the father of the human relations school.",
+            "D. none of the above."
+        ]
+    },
+    {
+        numb: 8,
+        question: " Elton Mayo is referred to as:",
+        answer: "C. the father of the human relations school.",
+        options: [
+            "A. the father of the modern management theory.",
+            "B. the father of scientific management.",
+            "C. the father of the human relations school.",
+            "D. none of the above."
+        ]
+    },
+    {
+        numb: 9,
+        question: " The “Hawthorne” studies concluded that:",
+        answer: "C. psychological incentives and type of supervision are very important.",
+        options: [
+            "A. there is an important need for teaching management.",
+            "B. financial incentives are very important.",
+            "C. psychological incentives and type of supervision are very important.",
+            "D. management is a social process of decision making."
+        ]
+    },
+    {
+        numb: 10,
+        question: " “(SWOT) Analysis” refers to:",
+        answer: "C. both the internal and external environments scanning.",
+        options: [
+            "A. the internal environment scanning.",
+            "B. the external environment scanning.",
+            "C. both the internal and external environments scanning.",
+            "D. none of the above."
+        ]
+    },
+    {
+        numb: 11,
+        question: " “Medium-term Planning” is called:",
+        answer: "B. intermediate planning.",
+        options: [
+            "A. strategic planning.",
+            "B. intermediate planning.",
+            "C. operational planning.",
+            "D. none of the above."
+        ]
+    },
+    {
+        numb: 12,
+        question: " One of the main characteristics of the effective goals and objectives is to be:",
+        answer: "D. all of the above.",
+        options: [
+            "A. accurate.",
+            "B. achievable.",
+            "C. divisible.",
+            "D. all of the above."
+        ]
+    },
+    
+];
